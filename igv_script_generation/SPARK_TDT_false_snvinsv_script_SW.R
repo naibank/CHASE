e71cbd23926d24a9899c6b8b3cb81bb95f770b88
Shania Wu <91928407+ShaniaWu@users.noreply.github.com>
2022-05-24 11:12:30 -0400

    Add files via upload

---
+++ igv_script_generation/SPARK_TDT_false_snvinsv_script_SW.R
@@ -0,0 +1,60 @@
+#setwd("/Users/shaniawu/Desktop/CHASE/SPARK_BA/IGV_SNV_check")
+
+## Get SPARK metadata
+
+SPARK_meta <- data.frame()
+
+for (i in 1:3) {
+  SPARK_metadata_path <- paste("../Data/SPARK_WGS_",i,"_metadata_relfixed.tsv",sep='')
+  meta <- read.delim(SPARK_metadata_path, stringsAsFactors = F)
+  meta$WGS_ver <- i # add col indicating SPARK WGS version number
+
+  SPARK_meta <- rbind(SPARK_meta, meta)
+}
+
+
+## Get SNV table
+IDs_and_pos <- fread('../ExtractSNVs/SPARK.parent.proband.SNVs.SNV1.CNV0.01.csv')
+
+write_script <- function(text, path='igv_false_snvinsv_script.sh', 
+                         append=T) {
+  write(text, path, append=append)
+}
+
+## TODO:
+# - only keep parents from CNV_SNV_table to be imported
+# - only keep nonsyn SNVs
+# - separate tables into transmitted and non-transmitted SNVs
+# - import meta to get child sample ID (neta) from parent sample ID (imported table)
+
+for (i in 1:nrow(IDs_and_pos)) {
+  write_script('new',)
+  write_script('genome hg38')
+  write_script('snapshotDirectory ./')
+  
+  row <- IDs_and_pos[i,]
+  ID <- row$X.Sample
+  child_ID <- 
+      # get from meta 
+      # meta$#Sample = parentID
+      # meta$sample = child ID
+  chr <- row$CHROM
+  pos <- row$POS
+  
+  # Get WGS version number for X.Sample
+  ver <- SPARK_meta$WGS_ver[which(SPARK_meta$Sample.ID ==  row$X.Sample)]
+  
+  load_path <- sprintf('https://bounce2.tcag.ca/tcag/mirrored/SPARK_WGS_%s/CRAM/%s.cram', ver, ID)
+  write_script(sprintf('load %s',load_path))
+  
+  load_path <- sprintf('https://bounce2.tcag.ca/tcag/mirrored/SPARK_WGS_%s/CRAM/%s.cram', ver, child_ID)
+  write_script(sprintf('load %s',load_path))
+  
+  location <- sprintf('%s:%s-%s',chr,pos-100,pos+100)
+  write_script(sprintf('goto %s',location))
+  
+  write_script('colorBy READ_STRAND')
+  
+  filename <- paste(ID,chr,pos,sep='_')
+  write_script(sprintf('snapshot %s',paste(filename,'.png',sep='')))
+}
