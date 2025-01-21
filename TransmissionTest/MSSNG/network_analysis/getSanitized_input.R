edfe7c189a3722a823400656e563557208ad9552
naibank <worrawat.bank@gmail.com>
2025-01-21 16:09:54 -0500

    Delete TransmissionTest directory

--- TransmissionTest/MSSNG/network_analysis/getSanitized_input.R
+++
@@ -1,7 +0,0 @@
-genes <- readLines("MSSNG.nonsyn.0.01perc.0.9pRec.txt")
-genes <- paste(genes, collapse="\t")
-
-input <- readLines("sanitized_input.txt")
-input[2] <- genes
-
-writeLines(input, "MSSNG_sanitized.txt")
