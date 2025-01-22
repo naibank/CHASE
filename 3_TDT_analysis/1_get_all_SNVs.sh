cfa448d68e7577e16b09b07f3d5170ed2bd87dce
Kara Han <92458614+karahyx@users.noreply.github.com>
2025-01-22 12:59:13 -0500

    Update 1_get_all_SNVs.sh

--- 3_TDT_analysis/1_get_all_SNVs.sh
+++ 3_TDT_analysis/1_get_all_SNVs.sh
@@ -5,27 +5,20 @@
 #SBATCH --tmp=100G
 #SBATCH -t 180:00:00
 
-root_dir=/hpf/largeprojects/tcagstor/scratch/kara.han/CHASE
-out_dir=$root_dir/data/SNV
-tool=$root_dir/script/1_get_SNVs.R
+root_dir="path_to_root_dir"
+out_dir="path_to_output_directory"
+tool="path_to_1_get_SNVs.R"
 
-# /hpf/largeprojects/tcagstor/tools/data/MSSNG/CG/variants/SNVs+indels/exonic+splicing 1738
-# /hpf/largeprojects/tcagstor/tools/data/MSSNG/ILMN/variants/SNVs+indels/exonic+splicing 9618
-# /hpf/largeprojects/tcagstor/tools/data/SSC/variants/SNVs+indels/exonic+splicing 9205
-# /hpf/largeprojects/tcagstor/tools/data/SPARK_WGS_1/variants/SNVs+indels/exonic+splicing 2629
-# /hpf/largeprojects/tcagstor/tools/data/SPARK_WGS_2/variants/SNVs+indels/exonic+splicing 2365
-# /hpf/largeprojects/tcagstor/tools/data/SPARK_WGS_3/variants/SNVs+indels/exonic+splicing 2867
-# /hpf/largeprojects/tcagstor/tools/data/SPARK_WGS_4/variants/SNVs+indels/exonic+splicing 3684
-infile_dir=/hpf/largeprojects/tcagstor/tools/data/MSSNG/CG/variants/SNVs+indels/exonic+splicing
-cohort=MSSNG_CG
+infile_dir="path_to_SNV_directory"
+cohort="cohort_name"
 cohort_outdir=$out_dir/$cohort
 mkdir $out_dir
 mkdir $cohort_outdir
 
 # Divide the total num of files into chunks
-num_smpl=$(find $infile_dir -name '*.tsv.gz' | wc -l)
+num_smpl=$(find $infile_dir -name '*.tsv.gz' | wc -l) # Change *.tsv.gz to corresponding file suffix
 echo "There are $num_smpl samples"
-num_per_chunk=150
+num_per_chunk=150 # Modify according to total number of samples
 a=$(($num_smpl/$num_per_chunk))
 b=$(($num_smpl%$num_per_chunk))
 echo "a is $a"
@@ -35,13 +28,13 @@ while [ "$i" -le "$a" ];
 do
     echo "i is $i"
     c=$(($i*$num_per_chunk))
-    files=$(find $infile_dir -name '*.tsv.gz' | head -n $c | tail -n $num_per_chunk) 
+    files=$(find $infile_dir -name '*.tsv.gz' | head -n $c | tail -n $num_per_chunk) # Change *.tsv.gz to corresponding file suffix
     snv_subset_outpath=$cohort_outdir/"$cohort"_snv_subset$i.txt
-    sbatch --export=tool=$tool,files="${files}",snv_subset_outpath=$snv_subset_outpath $root_dir/script/1_get_SNVs.sh
+    sbatch --export=tool=$tool,files="${files}",snv_subset_outpath=$snv_subset_outpath "path_to_1_get_SNVs.sh"
     i=$(($i+1))
 done
 echo "i is $i"
 files=$(find $infile_dir -name '*.tsv.gz' | tail -n $b)
 snv_subset_outpath=$cohort_outdir/"$cohort"_snv_subset$i.txt
-sbatch --export=tool=$tool,files="${files}",snv_subset_outpath=$snv_subset_outpath $root_dir/script/1_get_SNVs.sh
+sbatch --export=tool=$tool,files="${files}",snv_subset_outpath=$snv_subset_outpath "path_to_1_get_SNVs.sh"
 echo "Finished"
