91b10be2c058345b116b989aca4f7f64ae0d3a69
naibank <worrawat.bank@gmail.com>
2025-04-03 15:22:32 -0400

    Add files via upload

---
+++ prerun_family_QC/submit_jobs.sh
@@ -0,0 +1,7 @@
+sbatch 2_sh/SPARK_WGS_1.sh
+sbatch 2_sh/SPARK_WGS_2.sh
+sbatch 2_sh/SPARK_WGS_3.sh
+sbatch 2_sh/SPARK_WGS_4.sh
+sbatch 2_sh/SSC.sh
+sbatch 2_sh/MSSNG_ILMN.sh
+sbatch 2_sh/MSSNG_CG.sh
