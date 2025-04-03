db65fe9900bfff2fef3c1e7cd5e913dce0dc02f7
naibank <worrawat.bank@gmail.com>
2025-04-03 15:23:02 -0400

    Add files via upload

---
+++ prerun_family_QC/2_sh/MSSNG_ILMN.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name MSSNG_ILMN
+#SBATCH -N 1 -c 8
+#SBATCH --mem=48G
+#SBATCH --time=48:00:00
+
+module load R/3.5.1
+
+Rscript filterMosaicSVs.R MSSNG_ILMN
---
+++ prerun_family_QC/2_sh/SPARK_WGS_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name SPARK_WGS_2
+#SBATCH -N 1 -c 8
+#SBATCH --mem=48G
+#SBATCH --time=48:00:00
+
+module load R/3.5.1
+
+Rscript filterMosaicSVs.R SPARK_WGS_2
